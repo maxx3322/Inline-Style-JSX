0276460603e5e7d0d6d4d26e6522d7f09a843bf4
maxx3322 <maxdgeers@gmail.com>
2020-01-10 12:47:14 +0800

    final result
    adding inline styles to the jsx file so you can update them on the fly.  Sometimes it is better to do this rather than use the css document

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black",
+  outlineStyle: "groove",
+  outlineColor: "coral",
+  outlineWidth: "7px",
+  textTransform: "uppercase",
+  animation: "mymove 5s infinite"
+};
+
+ReactDOM.render(
+  <h1 style={customStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
